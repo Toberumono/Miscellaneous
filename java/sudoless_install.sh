e55f20e7a80834732dc29da72bd990324f689814
Toberumono <JoshuaLipstone@gmail.com>
2015-08-11 11:03:54 -0400

    Added force list directories for the jdk_path command.

--- java/sudoless_install.sh
+++ java/sudoless_install.sh
@@ -9,7 +9,7 @@ if [ "$yn" != "" ]; then
 fi
 tar -zxvf "$jdk_tar" -C "$jvm_path"
 
-jdk_path="$(ls $jvm_path/jdk* -t -1 | head -1)"
+jdk_path="$(ls -d $jvm_path/jdk* -t -1 | head -1)"
 echo "$jdk_path"
 ln -sf "$jdk_path" "$jvm_path/current"
 bash <(wget -qO - "https://raw.githubusercontent.com/Toberumono/Miscellaneous/master/java/append_paths.sh")
