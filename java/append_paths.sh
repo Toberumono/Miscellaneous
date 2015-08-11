fe97e305b4edc3cfafb278fbdcfd13b4b08d3316
Toberumono <JoshuaLipstone@gmail.com>
2015-08-11 10:30:35 -0400

    Added java/append_paths.sh and java/sudoless_install.sh. These scripts should allow for the installation of a JDK without use of sudo.

---
+++ java/append_paths.sh
@@ -0,0 +1,41 @@
+#Add Java into PATH and MANPATH
+jvm_path='$HOME/jvm'
+jvm_current_path=$jvm_path"/current"
+
+client="JDK"
+should_reopen=""
+
+update_rc() {
+	added=false
+	for p in $2; do
+		add_path=$(grep -F -e 'export '$p "$1")
+		if ( ! $add_path); then
+			echo 'export '$p" is already in $1"
+			continue;
+		fi
+		if ( ! $added ); then
+			echo "" >> "$1"
+			echo "# This adds the necessary $client paths." >> "$1"
+			[ "$should_reopen" == "" ] && should_reopen="$1" || should_reopen=$should_reopen" $1"
+			added=true
+		fi
+		echo "Adding 'export '$p to $1"; echo 'export '$p >> "$1"
+	done
+}
+
+jdk_path="$(ls $jvm_path/jdk* -t -1 | head -1)"
+ln -sfr "$jdk_path" "$jvm_current_path"
+
+paths=('PATH="'$jvm_current_path'/bin:$PATH"', 'MANPATH="'$jvm_current_path'/man:$MANPATH"')
+
+[ -e "$HOME/.bashrc" ] && update_rc "$HOME/.bashrc" $paths
+[ -e "$HOME/.zshrc" ] && update_rc "$HOME/.zshrc" $paths
+
+for var in "$@"; do
+	[ -e "$var" ] && update_rc "$var" $paths
+done
+
+if [ "$should_reopen" != "" ]; then
+	echo "Modified: $should_reopen"
+	echo "Please restart your terminal session for these changes to take effect (open a new window and close this one once you are done with the information in it)."
+fi
---
+++ java/sudoless_install.sh
@@ -0,0 +1,11 @@
+#Install the most recent JDK without sudo
+jvm_path='$HOME/jvm'
+
+mkdir -p "$jvm_path"
+jdk_tar="$(ls $(pwd)/jdk* -t -1 | head -1)"
+read -p "Installing the JDK in $jdk_tar.  Proceed? [Press Enter to continue, press any other key to quit]" yn
+if [ "$yn" != "" ]; then
+	exit 0;
+fi
+tar -zxvf "$jdk_tar" -C "$jvm_path"
+bash <(wget -qO - "https://raw.githubusercontent.com/Toberumono/Miscellaneous/master/java/append_paths.sh")
