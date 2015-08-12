afa1984180d085454a0ae167d6ad2ad903d01b83
Toberumono <JoshuaLipstone@gmail.com>
2015-08-12 10:55:39 -0400

    Debugging time.

--- linuxbrew/automatic_install.sh
+++ linuxbrew/automatic_install.sh
@@ -1,17 +1,19 @@
 #This script requires sudo privileges
 
+echo "test!"
+
 if [ "$(which brew)" != "" ]; then
 	echo "Linuxbrew is already installed."
 	exit 0
 fi
 
 if [ "$(which apt-get)" != "" ]; then
-	sudo apt-get install build-essential curl git m4 ruby texinfo libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
+	apt-get install build-essential curl git m4 ruby texinfo libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
 elif [ "$(which yum)" != "" ]; then
-	sudo yum groupinstall 'Development Tools' && sudo yum install curl git irb m4 ruby texinfo bzip2-devel curl-devel expat-devel ncurses-devel zlib-devel
+	yum groupinstall 'Development Tools' && yum install curl git irb m4 ruby texinfo bzip2-devel curl-devel expat-devel ncurses-devel zlib-devel
 else
 	echo "Unable to find a valid package manager.  Attempting to proceed."
 fi
 
 . <(wget -qO - "https://raw.githubusercontent.com/Toberumono/Miscellaneous/master/general/unsudo.sh")
-bash <(wget -qO - "https://raw.githubusercontent.com/Toberumono/Miscellaneous/master/linuxbrew/sudoless_install.sh")
+$unsudo bash <(wget -qO - "https://raw.githubusercontent.com/Toberumono/Miscellaneous/master/linuxbrew/sudoless_install.sh")
