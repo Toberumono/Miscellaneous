d1052353a9e0363f55bb8eec1c5492e3abd1a368
Toberumono <JoshuaLipstone@gmail.com>
2015-08-15 15:15:28 -0400

    This should fix the paths.

--- ncl-ncarg/sudoless_install.sh
+++ ncl-ncarg/sudoless_install.sh
@@ -2,9 +2,10 @@
 #Author: Toberumono (https://github.com/Toberumono)
 
 ncl_tar="$(ls $(pwd)/ncl_ncarg* -t -1 | head -1)"
-ncl_path="$HOME/.ncl-ncarg/$(echo $ncl_tar | grep -oE 'ncl_ncarg-([0-9]+\.)*[0-9]+')"
+ncl_path="$HOME/.ncl-ncarg"
+ncl_installed_path="$ncl_path/$(echo $ncl_tar | grep -oE 'ncl_ncarg-([0-9]+\.)*[0-9]+')"
 
-mkdir -p "$ncl_path"
+mkdir -p "$ncl_installed_path"
 read -n1 -p "Installing NCL-NCARG in $ncl_path using $ncl_tar.  Proceed? [Press Enter to continue, press any other key to quit]" yn
 if [ "$yn" != "" ]; then
 	echo "" #Echo a newline for neatness' sake
@@ -12,6 +13,5 @@ if [ "$yn" != "" ]; then
 fi
 tar -zxvf "$ncl_tar" -C "$ncl_path"
 
-ncl_installed_path="$(ls -d $ncl_path/ncl* -t -1 | head -1)"
 ln -sf "$ncl_installed_path" "$ncl_path/current"
 bash <(wget -qO - "https://raw.githubusercontent.com/Toberumono/Miscellaneous/master/ncl-ncarg/append_paths.sh")
