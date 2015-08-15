093973b6400ac3f7427f9113c141260c6e4b0297
Toberumono <JoshuaLipstone@gmail.com>
2015-08-14 21:50:47 -0400

    Another path fix.

--- ant/sudoless_install.sh
+++ ant/sudoless_install.sh
@@ -4,7 +4,7 @@
 ant_path="$HOME/.ant"
 
 mkdir -p "$ant_path"
-ant_tar="$(ls $(pwd)/ant* -t -1 | head -1)"
+ant_tar="$(ls $(pwd)/apache-ant* -t -1 | head -1)"
 read -n1 -p "Installing Apache Ant in $ant_path using $ant_tar.  Proceed? [Press Enter to continue, press any other key to quit]" yn
 if [ "$yn" != "" ]; then
 	echo "" #Echo a newline for neatness' sake
