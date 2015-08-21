041668c7dedfb28925f3c3bf7c8ce03de7fe32f7
Toberumono <JoshuaLipstone@gmail.com>
2015-08-20 21:13:53 -0400

    Added in Darwin checks.

--- ncl-ncarg/append_paths.sh
+++ ncl-ncarg/append_paths.sh
@@ -13,13 +13,22 @@ ncl_current_path="$ncl_path/current"
 
 path_path='PATH="$NCARG_ROOT/bin:$PATH"'
 #Okay, this line is /really/ long.  Basically, on Darwin systems (OSX), this gets the path to the libgfortran.3.dylib's containing folder.  This should /not/ be called on Linux systems.
+#This basically takes advantage of a pattern in how the gfortran executable is linked by Homebrew.  It should work with other installations, and, even if it doesn't, it won't break them.
 fallback_path='DYLD_FALLBACK_LIBRARY_PATH="$(echo $(echo $(echo $(which gfortran | sed "s/\/[^\/]*$/\//")$(readlink $(which gfortran)) | sed "s/\/[^\/]*$/\//")../lib/gcc/$(readlink $(which gfortran | sed "s/\/[^\/]*$/\//")$(readlink $(which gfortran)) | grep -oE '\''([0-9]+\.)*[0-9]+$'\'')) | sed '\''s/\([^\/]*\)\/\.\.\///g'\''):$DYLD_FALLBACK_LIBRARY_PATH"'
 
-update_rc "NCL-NCARG" "$profile" "NCARG_ROOT=$ncl_current_path" "$path_path" "$fallback_path"
+if [ "$(uname -s)" == "Darwin" ]; then
+	update_rc "NCL-NCARG" "$profile" "NCARG_ROOT=$ncl_current_path" "$path_path" "$fallback_path"
 
-for var in "$@"; do
-	update_rc "NCL-NCARG" "$var" "NCARG_ROOT=$ncl_current_path" "$path_path" "$fallback_path"
-done
+	for var in "$@"; do
+		update_rc "NCL-NCARG" "$var" "NCARG_ROOT=$ncl_current_path" "$path_path" "$fallback_path"
+	done
+else
+	update_rc "NCL-NCARG" "$profile" "NCARG_ROOT=$ncl_current_path" "$path_path"
+
+	for var in "$@"; do
+		update_rc "NCL-NCARG" "$var" "NCARG_ROOT=$ncl_current_path" "$path_path"
+	done
+fi
 
 #The should_reopen variable is added by the update_rc.sh script.
 if [ "$should_reopen" != "" ]; then
