78c907cfd6a1aeff7e790222366f045c54882412
Toberumono <JoshuaLipstone@gmail.com>
2015-08-22 16:07:55 -0400

    Attempting to handle the fallback_path better.

--- ncl-ncarg/brewed_path_fix.sh
+++ ncl-ncarg/brewed_path_fix.sh
@@ -10,11 +10,11 @@
 #Okay, this line is /really/ long.  Basically, on Darwin systems (OSX), this gets the path to the libgfortran.3.dylib's containing folder.  This should /not/ be called on Linux systems.
 #This basically takes advantage of a pattern in how the gfortran executable is linked by Homebrew.  It should work with other installations, and, even if it doesn't, it won't break them.
 #This also has the advantage of automatically picking the library from whichever build of gfortran is currently in use.
-fallback_path='export DYLD_FALLBACK_LIBRARY_PATH="$(echo $(echo $(echo $(which gfortran | sed "s/\/[^\/]*$/\//")$(readlink $(which gfortran)) | sed "s/\/[^\/]*$/\//")../lib/gcc/$(readlink $(which gfortran | sed "s/\/[^\/]*$/\//")$(readlink $(which gfortran)) | grep -oE '\''([0-9]+\.)*[0-9]+$'\'')) | sed '\''s/\([^\/]*\)\/\.\.\///g'\''):$DYLD_FALLBACK_LIBRARY_PATH"'
 
 ncl_current="$(brew --prefix)/ncl-current"
 ncarg_root_path="export NCARG_ROOT=$ncl_current"
 path_path='export PATH=$NCARG_ROOT/bin:$PATH'
+fallback_path='export DYLD_FALLBACK_LIBRARY_PATH=$(dirname $(gfortran --print-file-name libgfortran.3.dylib)):$DYLD_FALLBACK_LIBRARY_PATH'
 
 update_rc "Brewed NCAR-NCL" "$profile" "$ncarg_root_path" "$path_path" "$fallback_path"
 
