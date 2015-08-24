fa4cfdbb7f8741336cf92b1f7b9ef0dc23f3362d
Toberumono <JoshuaLipstone@gmail.com>
2015-08-24 09:49:40 -0400

    Added version_test.sh to common.

--- ant/automatic_install.sh
+++ ant/automatic_install.sh
@@ -5,37 +5,29 @@
 #Get the command to use when grabbing subscripts from GitHub.
 [ "$(which wget)" == "" ] && pull_command="curl -fsSL" || pull_command="wget -qO -"
 . <($pull_command "https://raw.githubusercontent.com/Toberumono/Miscellaneous/master/common/unsudo.sh")
+. <($pull_command "https://raw.githubusercontent.com/Toberumono/Miscellaneous/master/common/version_test.sh")
 
 #Grab the most recent Ant version from the changelog.
 ant_version_pattern='([0-9]+\.)*[0-9]+$'
 ant_version="$( ($pull_command http://supergsego.com/apache//ant/README.html) | grep -E '^Changes' | grep -oE $ant_version_pattern)"
-echo "Downloading Apache-Ant version $ant_version"
+echo "Available Ant version: $ant_version"
 
-should_install=0
+should_install=1
 if [ "$(which ant)" != "" ]; then
 	current_version="$(ant -version | grep -oE '^.*?([0-9]+\.)*[0-9]+' | grep -oE -m 1 '([0-9]+\.)*[0-9]+')"
-	IFS='.' read -ra cv_arr <<< "$ant_version"
-	IFS='.' read -ra av_arr <<< "$current_version"
-	i="0"
-	if [ "${#cv_arr[@]}" -ne "${#av_arr[@]}" ]; then
-		should_install=2
-	fi
-	while [ "$should_install" -eq "0" ] && [ "$i" -lt "${#cv_arr[@]}" ]; do
-		if [ "${cv_arr[$i]}" -gt "${av_arr[$i]}" ]; then
-			echo "A newer version of Apache Ant (version $current_version) is already installed.  Please uninstall that version before re-running this script."
-			exit 0
-		elif [ "${cv_arr[$i]}" -lt "${av_arr[$i]}" ]; then
-			should_install=1
-		fi
-		(( i = i + 1 ))
-	done
+	echo "Current Apache-Ant version: $current_version"
+	should_install="$(version_test $current_version $ant_version)"
 else
-	should_install=2
+	echo "Current Apache-Ant version: none"
+	should_install="-1"
 fi
 if [ "$should_install" -eq "0" ]; then
-	echo "Apache Ant version $current_version is already installed.  Nothing to do here.  Exiting."
+	echo "A newer version of Ant, $current_version, is already installed.  Nothing to do here.  Exiting."
 	exit 0
 elif [ "$should_install" -eq "1" ]; then
+	echo "Apache Ant version $current_version is already installed.  Nothing to do here.  Exiting."
+	exit 0
+elif [ "$should_install" -eq "2" ]; then
 	echo "Updating Apache Ant version $current_version to version $ant_version."
 else
 	echo "Installing Apache Ant version $ant_version."
---
+++ common/version_test.sh
@@ -0,0 +1,38 @@
+#Determines whether a given program is up to date given the installed version and the available version.
+#Author: Toberumono (https://github.com/Toberumono)
+
+#Takes the following arguments:
+#  1: the current version as a string (numbers separated by '.')
+#  2: the available version as a string (numbers separated by '.')
+#Returns: 0 if the current version is newer than the available version
+#           or it's version code is longer
+#         1 if the versions are equal
+#         2 if the available version is newer than the current version
+#           or it's version code is longer
+#All Returns occur via echo
+version_test() {
+	local cv_arr=""
+	local av_arr=""
+	IFS='.' read -ra cv_arr <<< "$1"
+	IFS='.' read -ra av_arr <<< "$2"
+	if [ "${#cv_arr[@]}" -gt "${#av_arr[@]}" ]; then
+		echo "0"
+		return 0;
+	elif [ "${#cv_arr[@]}" -lt "${#av_arr[@]}" ]; then
+		echo "2"
+		return 0;
+	fi
+	local i="0"
+	while [ "$i" -lt "${#cv_arr[@]}" ]; do
+		if [ "${cv_arr[$i]}" -gt "${av_arr[$i]}" ]; then
+			echo "0"
+			return 0;
+		elif [ "${cv_arr[$i]}" -lt "${av_arr[$i]}" ]; then
+			echo "2"
+			return 0;
+		fi
+		(( i = i + 1 ))
+	done
+	echo "1"
+	return 0;
+}
