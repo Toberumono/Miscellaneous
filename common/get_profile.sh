3803440589152f77ecb96b7c2536a5bb19d5dc3f
Toberumono <JoshuaLipstone@gmail.com>
2015-08-25 11:37:45 -0500

    Added a failsafe to get_profile.sh for if a full path is returned.

--- common/get_profile.sh
+++ common/get_profile.sh
@@ -1,6 +1,6 @@
 #Grabs the profile file for the current shell taking the OS into account (e.g. ~/.bashrc or ~/.bash_profile)
 #Author: Toberumono (https://github.com/Toberumono)
 
-shell_exec="$(ps -o comm= -p $$ | sed -e 's/-\{0,1\}\(.*\)/\1/')"
+shell_exec="$(ps -o comm= -p $$ | sed -e 's/-\{0,1\}\(.*\)/\1/' | grep -oE '[^/]*$')"
 [ "$(uname -s)" == "Darwin" ] && profile="$HOME/.${shell_exec}_profile" || profile="$HOME/.${shell_exec}rc"
 unset shell_exec
