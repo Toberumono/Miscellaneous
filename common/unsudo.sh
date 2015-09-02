52b77821547444585ca1b7f1e1aa1a811fd484eb
Toberumono <JoshuaLipstone@gmail.com>
2015-09-02 15:28:04 -0500

    Corrected a description comment.

--- common/unsudo.sh
+++ common/unsudo.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-#A support function used to append export commands to .bashrc, .zshrc, etc.
+#A support function to determine the correct user to use when calling
+#a step without sudo permissions from within a script run as sudo.
 #Author: Toberumono (https://github.com/Toberumono)
 
 [ $SUDO_USER ] && unsudo="sudo -u $SUDO_USER" || unsudo=""
